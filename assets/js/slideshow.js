3c7dc8bdd3c1fe58446c23e77cb7dd6747454337
PoiKoi <tristan.li.846@gmail.com>
2021-09-23 20:41:05 +0930

    Update slideshow.js

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -8,7 +8,6 @@ var images = ['banner1.png', 'banner2.png', 'banner3.png','banner4.png'];
 
 function slideshow() {
 	document.getElementById('banner').setAttribute("style", "background-image: url(" + dir + images[imgCount] + ");");
-   	console.log(imgCount);
 	imgCount++;
 	if(imgCount >= 4){
 		imgCount = 0;

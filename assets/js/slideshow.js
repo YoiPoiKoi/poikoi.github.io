a418450d4faf8078bb8bd56a4eb92a2f13ac9916
PoiKoi <tristan.li.846@gmail.com>
2021-09-23 23:14:07 +0930

    Add files via upload

--- assets/js/slideshow.js
+++ assets/js/slideshow.js
@@ -3,11 +3,12 @@ slideshow();
 }, 5000);
 
 var imgCount = 0;
-var dir = 'assets/img/';
+var dir = 'file:///C:/Users/Brendon/Desktop/github/PKWebpage/assets/img/';
 var images = ['banner1.png', 'banner2.png', 'banner3.png','banner4.png'];
 
 function slideshow() {
 	document.getElementById('banner').setAttribute("style", "background-image: url(" + dir + images[imgCount] + ");");
+   	console.log(imgCount);
 	imgCount++;
 	if(imgCount >= 4){
 		imgCount = 0;
